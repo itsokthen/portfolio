6c0ec66630f147cd8b972b1a06c01e3794697436
itsokthen <cades117@gmail.com>
2024-10-29 14:23:48 -0500

    project components have expanding button to show more content. TODO: more animations

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -1,32 +1,55 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "../ProjectComponent.module.css";
 
 export default function ProjectComponent(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => setIsExpanded(!isExpanded);
+
   return (
-    <div className={styles.projectComponent}>
+    <div
+      className={`${styles.projectComponent} ${
+        isExpanded ? styles.expanded : ""
+      }`}
+    >
       <img src={props.img} alt="project" className={styles["skill-image"]} />
       <h3 className={styles.projectTitle}>{props.title}</h3>
       <p className={styles.projectTags}>{props.tags}</p>
-      <p>{props.description}</p>
-      <p>
-        <strong>Contribution: </strong>
-        {props.contribution}
-      </p>
-      <p>
-        <strong>What I learned: </strong>
-        {props.learned}
-      </p>
+      <div className={styles.projectTextContainer}>
+        <p>{props.description}</p>
+        <p>
+          <strong>Contribution: </strong>
+          {props.contribution}
+        </p>
+        <p>
+          <strong>What I learned: </strong>
+          {props.learned}
+        </p>
+      </div>
       <div className={styles.projectLinks}>
-        {props.liveurl && (
-          <a href={props.liveurl} target="_blank" rel="noopener noreferrer">
-            <button className="button">Live</button>
-          </a>
-        )}
-        {props.github && (
-          <a href={props.github} target="_blank" rel="noopener noreferrer">
-            <button className="button">GitHub</button>
-          </a>
-        )}
+        <div className={styles.buttonContainer}>
+          <button
+            onClick={toggleExpand}
+            className={`${styles.expandButton} ${
+              isExpanded ? styles.expanded : styles.collapsed
+            }`}
+          >
+            {isExpanded ? "▲ Less" : "▼ More"}
+          </button>
+        </div>
+        <div className={styles.buttons}>
+          {props.liveurl && (
+            <a href={props.liveurl} target="_blank" rel="noopener noreferrer">
+              <button className="button">Live</button>
+            </a>
+          )}
+          {props.github && (
+            <a href={props.github} target="_blank" rel="noopener noreferrer">
+              <button className="button">GitHub</button>
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
